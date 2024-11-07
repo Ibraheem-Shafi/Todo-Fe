309f7172ca564ce04c6ff18b370f7f6d6afa25ff
ibraheem <ibraheem45622@gmail.com>
2024-11-07 15:52:14 +0500

    first commit

--- src/Server/helper.js
+++ src/Server/helper.js
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://backend-azure-ten.vercel.app/"
+export const BACKEND_URL = "https://todo-be-rose.vercel.app/"
 
-export const BACKEND_URL = "http://localhost:5000/"
+// export const BACKEND_URL = "http://localhost:5000/"
